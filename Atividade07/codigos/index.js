b61191359403ae2d35f7f3035e4a83003a2abc3f
CGdiego <266080606+CGdiego@users.noreply.github.com>
2026-09-21 22:19:39 -0300

    Added login system

--- Atividade07/codigos/index.js
+++ Atividade07/codigos/index.js
@@ -1,3 +1,8 @@
+const cadastrados = [
+    "admin@gmail.com",
+    "user@gmail.com"
+];
+
 const inptEmail = document.getElementById("inptEmail");
 const inptYear = document.getElementById("inptYear");
 const btnEntrar = document.getElementById("btnEntrar");
@@ -11,6 +16,8 @@ function validate(email, ano){
         alert("Adicione um e-mail.");
     else if (ano == "")
         alert("Adicione um ano de nascimento.");
+    else if (!cadastrados.includes(email))
+        alert("Insira um e-mail válido da lista.");
     else if (2026 - ano < 18)
         alert("Apenas maiores de 18 anos podem entrar.");
     else
